488d9f752cf77983a5ce40c68c53f7f1c5567054
mahmod <mahmoudelattar800@gmail.com>
2020-06-22 04:20:26 +0200

    working on get data from JSON file dynamically

--- apps/app.js
+++ apps/app.js
@@ -9,7 +9,7 @@ const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
 const productsDOM = document.querySelector('.products-center');
-
+ 
 
 // Cart 
 let cart = [];
@@ -18,8 +18,16 @@ let cart = [];
 class Products{
     async getProducts(){
         try{
-            let result = await fetch('products.json')
-            return result;
+            let result = await fetch("http://localhost/E-Commerce-Project/products.json");
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const {title, price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {title, price, id, image};
+            })
+            return products;
         } catch(error) {
             console.log(error);
         }
@@ -28,12 +36,55 @@ class Products{
 
 //display products
 class UI {
-
+    displayProducts(products){
+        let results = '';
+        products.forEach(product => {
+            results += `
+            <!-- single product-->
+            <article class="product">
+                <div class="img-container">
+                    <a href="https://amzwatches.com/product/bos-mens-watch-aw748dqo/" target="_blank">
+                        <img src=${product.image} alt ="" class="product-img">
+                    </a>
+                    <button class="bag-btn" data-id= ${product.id}>
+                        <i class="fas fa-shopping-cart"></i>
+                        add to the cart
+                    </button>
+                    <button class="bag-btn1" data-id= "1">
+                        <i class="far fa-heart"></i>
+                    </button>
+                </div>
+                <h3>${product.title}</h3>
+                <h4>${product.price}</h4>
+            </article>
+            <!-- end of single product-->
+            `
+        });
+        productsDOM .innerHTML = results;
+    }
+    getBagButtons(){
+        const btns = [...document.querySelectorAll('.bag-btn')];
+        btns.forEach(button =>{
+            let id = button.dataset.id;
+            let incart = cart.find(item => item.id === id);
+            if(incart){
+                button.innerText = "Already in your cart";
+                button.disabled = true;
+            } else {
+                button.addEventListener('click', (e)=>{
+                    e.target.innerText = "Already in your cart";
+                    e.target.disabled = true;
+                })
+            }
+        });
+    }
 }
 
 //local storage
 class Storage {
-
+    static saveProduct(products){
+        localStorage.setItem("products", JSON.stringify(products));
+    }
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
@@ -41,6 +92,12 @@ document.addEventListener("DOMContentLoaded",()=>{
     const products = new Products();
 
     // get all products
-    products.getProducts().then(data => console.log(data));
+    products.getProducts().then(products => {ui.displayProducts
+        (products)
+        Storage.saveProduct(products)
+    }).then(() => {
+        ui.getBagButtons();
+    });
 });
+
 
